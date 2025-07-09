6449f1f6a3ec889e7525c75ad6c8c84c73eb632a
cloudykraken <84597657+cloudykraken@users.noreply.github.com>
2025-07-09 11:26:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 
+function login() {
+  const username = "peter.smith@gmail.com"
+  const password = "238hadfjkdf"
+
+  if (username == getUserNameFromdB() && password === getPasswordFromDb()) {
+       console.log("You are logged in!")
+  } else {
+       console.log("Incorrect username or password!"
+  }
+}
